34aeac5c87bccea9b3f197a501d4c634586190a2
Brijesh Naik <ec2-user@ip-172-31-54-253.ec2.internal>
2020-10-20 06:30:50 +0000

    positioning google button

--- scripts/GoogleButton.jsx
+++ scripts/GoogleButton.jsx
@@ -14,6 +14,7 @@ import { Content } from './Content';
         'address': name+' ('+email+') :',
     });
     
+    
     console.log('Sent the name ' + name +' and email '+email+ ' to the server!');
     ReactDOM.render(<Content />, document.getElementById('content'));
 }
