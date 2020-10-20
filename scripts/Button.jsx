4123c442b1c040e66f70039e23239e2acba26046
Brijesh Naik <ec2-user@ip-172-31-54-253.ec2.internal>
2020-10-20 07:27:10 +0000

    remove username stuff from m1

--- scripts/Button.jsx
+++ scripts/Button.jsx
@@ -2,22 +2,15 @@ import * as React from 'react';
 import { Socket } from './Socket';
 
 function handleSubmit(event) {
-    let username = document.getElementById("username");
     let input = document.getElementById("input");
-    
-    Socket.emit('new username', {
-        'address': username.value+': ',
-    });
-    username.value
     
     Socket.emit('new input', {
         'address': input.value,
     });
     input.value
     
-    console.log('Sent the username and input data ' +username.value+' '+'and message: '+ input.value + ' to the server!');
+    console.log('Sent the input data '+ input.value + ' to the server!');
     input.value = ''
-    username.value=''
     
     event.preventDefault();
 }
@@ -27,8 +20,6 @@ export function Button() {
         <form onSubmit={handleSubmit}>
             <input id="input" placeholder="Type a message"></input>
             <button>Send</button>
-            <p>Username: </p>
-            <input id="username" placeholder="Username"></input>
         </form>
     );
 }
